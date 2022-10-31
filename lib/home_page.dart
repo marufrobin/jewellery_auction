3709fd106f0a3227437f09b36f28aa88dadc58ab
Maruf Robin <maruf.robin@northsouth.edu>
2022-11-01 04:51:18 +0600

    gridview.count allmost done

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -12,95 +12,196 @@ class HomePage extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Color(0xffDACBA4),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            CustomAppBar(),
-            Text(
-              "Let's Make A Bid",
-              style: TextStyle(
-                  fontFamily: "Noto", fontSize: 20, color: Colors.black54),
-            ),
-            SearchBar(),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "Categories",
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CustomAppBar(),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  "Let's Make A Bid",
                   style: TextStyle(
-                      fontSize: 24,
-                      fontFamily: "Noto",
-                      fontWeight: FontWeight.w800),
+                      fontFamily: "Noto", fontSize: 20, color: Colors.black54),
                 ),
-                Text(
-                  "See All",
-                  style: TextStyle(
-                      fontSize: 18,
-                      fontFamily: "Noto",
-                      color: Colors.orange.shade600,
-                      fontWeight: FontWeight.w800),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              SearchBar(),
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Categories",
+                      style: TextStyle(
+                          fontSize: 24,
+                          fontFamily: "Noto",
+                          fontWeight: FontWeight.w800),
+                    ),
+                    Text(
+                      "See All",
+                      style: TextStyle(
+                          fontSize: 18,
+                          fontFamily: "Noto",
+                          color: Colors.orange.shade600,
+                          fontWeight: FontWeight.w800),
+                    ),
+                  ],
                 ),
-              ],
-            ),
-            Container(
-              height: MediaQuery.of(context).size.width * 0.4,
-              // color: Colors.grey,
-              child: ListView.separated(
-                  scrollDirection: Axis.horizontal,
-                  itemBuilder: ((context, index) {
-                    return Container(
-                      padding: EdgeInsets.all(10),
-                      margin: EdgeInsets.only(left: 16),
-                      width: 140,
-                      decoration: BoxDecoration(
-                          color: Color(0xffF2E8CE),
-                          borderRadius: BorderRadius.circular(8)),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Expanded(
-                              flex: 2,
-                              child: Image.asset(
-                                "${modelData[index].productImageName}",
-                                fit: BoxFit.cover,
-                              )),
-                          Text(
-                            "${modelData[index].productCategories}",
-                            style: TextStyle(
-                                fontSize: 18,
-                                fontFamily: "Noto",
-                                fontWeight: FontWeight.w600),
-                          )
-                        ],
-                      ),
-                    );
-                  }),
-                  separatorBuilder: (context, index) => SizedBox(
-                        width: 10,
-                      ),
-                  itemCount: modelData.length),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "Categories",
-                  style: TextStyle(
-                      fontSize: 24,
-                      fontFamily: "Noto",
-                      fontWeight: FontWeight.w800),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Container(
+                height: MediaQuery.of(context).size.width * 0.4,
+                // color: Colors.grey,
+                child: ListView.separated(
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: ((context, index) {
+                      return Container(
+                        padding: EdgeInsets.all(10),
+                        margin: EdgeInsets.only(left: 16),
+                        width: 140,
+                        decoration: BoxDecoration(
+                            color: Color(0xffF2E8CE),
+                            borderRadius: BorderRadius.circular(8)),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Expanded(
+                                flex: 2,
+                                child: Image.asset(
+                                  "${modelData[index].productImageName}",
+                                  fit: BoxFit.cover,
+                                )),
+                            Text(
+                              "${modelData[index].productCategories}",
+                              style: TextStyle(
+                                  fontSize: 18,
+                                  fontFamily: "Noto",
+                                  fontWeight: FontWeight.w600),
+                            )
+                          ],
+                        ),
+                      );
+                    }),
+                    separatorBuilder: (context, index) => SizedBox(
+                          width: 10,
+                        ),
+                    itemCount: modelData.length),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Trending Auctions",
+                      style: TextStyle(
+                          fontSize: 24,
+                          fontFamily: "Noto",
+                          fontWeight: FontWeight.w800),
+                    ),
+                    Text(
+                      "See All",
+                      style: TextStyle(
+                          fontSize: 18,
+                          fontFamily: "Noto",
+                          color: Colors.orange.shade600,
+                          fontWeight: FontWeight.w800),
+                    ),
+                  ],
                 ),
-                Text(
-                  "See All",
-                  style: TextStyle(
-                      fontSize: 18,
-                      fontFamily: "Noto",
-                      color: Colors.orange.shade600,
-                      fontWeight: FontWeight.w800),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: GridView.count(
+                  physics: NeverScrollableScrollPhysics(),
+                  // scrollDirection: Axis.horizontal,
+                  crossAxisCount: 2,
+                  shrinkWrap: true,
+                  children: List.generate(
+                      modelData.length,
+                      (index) => Container(
+                            margin: EdgeInsets.all(4),
+                            padding: EdgeInsets.all(8),
+                            decoration: BoxDecoration(
+                                color: Colors.orangeAccent,
+                                borderRadius: BorderRadius.circular(16)),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Expanded(
+                                    flex: 1,
+                                    child: Image.asset(
+                                      "${modelData[index].productImageName}",
+                                      fit: BoxFit.cover,
+                                    )),
+                                Text(
+                                  "${modelData[index].productName}",
+                                  style: TextStyle(
+                                    fontFamily: "Noto",
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Row(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          "Current Bid",
+                                          style: TextStyle(
+                                              fontFamily: "Noto",
+                                              fontSize: 14,
+                                              fontWeight: FontWeight.bold,
+                                              color: Colors.grey.shade700),
+                                        ),
+                                        Text(
+                                          "\$${modelData[index].price}",
+                                          style: TextStyle(
+                                              fontFamily: "Noto",
+                                              fontSize: 14,
+                                              fontWeight: FontWeight.bold,
+                                              color: Colors.grey.shade700),
+                                        ),
+                                      ],
+                                    ),
+                                    Container(
+                                      margin: EdgeInsets.all(2),
+                                      padding: EdgeInsets.all(8),
+                                      decoration: BoxDecoration(
+                                        color: Colors.black,
+                                        borderRadius: BorderRadius.circular(4),
+                                      ),
+                                      child: Text(
+                                        "Bid Now",
+                                        style: TextStyle(color: Colors.white),
+                                      ),
+                                    ),
+                                  ],
+                                )
+                              ],
+                            ),
+                          )),
                 ),
-              ],
-            ),
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/widgets/custom_app_bar.dart
+++ lib/widgets/custom_app_bar.dart
@@ -7,38 +7,41 @@ class CustomAppBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Text(
-          "Hey Robin",
-          style: TextStyle(
-              fontSize: 40, fontFamily: "Noto", fontWeight: FontWeight.w800),
-        ),
-        Container(
-            padding: EdgeInsets.all(4),
-            width: 50,
-            height: 50,
-            decoration: BoxDecoration(
-                color: Colors.orange.shade100,
-                borderRadius: BorderRadius.circular(8)),
-            child: Stack(children: [
-              Icon(
-                Icons.notifications_none_outlined,
-                color: Colors.orange.shade400,
-                size: 40,
-              ),
-              Positioned(
-                top: 2,
-                right: 10,
-                child: CircleAvatar(
-                  radius: 6,
-                  backgroundColor: Colors.red,
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            "Hey Robin",
+            style: TextStyle(
+                fontSize: 40, fontFamily: "Noto", fontWeight: FontWeight.w800),
+          ),
+          Container(
+              padding: EdgeInsets.all(4),
+              width: 50,
+              height: 50,
+              decoration: BoxDecoration(
+                  color: Colors.orange.shade100,
+                  borderRadius: BorderRadius.circular(8)),
+              child: Stack(children: [
+                Icon(
+                  Icons.notifications_none_outlined,
+                  color: Colors.orange.shade400,
+                  size: 40,
                 ),
-              )
-            ]))
-      ],
+                Positioned(
+                  top: 2,
+                  right: 10,
+                  child: CircleAvatar(
+                    radius: 6,
+                    backgroundColor: Colors.red,
+                  ),
+                )
+              ]))
+        ],
+      ),
     );
   }
 }
